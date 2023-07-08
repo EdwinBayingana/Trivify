504da593a1b24c4a569a22febbe497b58e2b802a
EdwinBayingana <bayinganaedwin@gmail.com>
2023-07-08 21:33:13 +0200

    install required pckages

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useState } from 'react';
+import { quiz } from '../data.js';
+
+const page = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+
+  return (
+    <div>
+      <h1 className="text-[25px]">Quiz Page</h1>
+      <div>
+        <h3>
+          Question: {activeQuestion + 1}
+          <span>/5</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default page;
