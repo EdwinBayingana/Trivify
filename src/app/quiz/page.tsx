bbe7656b9e5f1e8a3ad4e87f70a6195dfc514ce9
EdwinBayingana <bayinganaedwin@gmail.com>
2023-07-08 22:25:53 +0200

    style the quiz form

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -17,14 +17,73 @@ const page = () => {
   const { questions } = quiz;
   const { question, answers, correctAnswer } = questions[activeQuestion];
 
+  const onAnswerSelected = (answer: string, index: number) => {
+    setChecked(true);
+    setSelectedAnswerIndex(index);
+
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log('true');
+    } else {
+      setSelectedAnswer(false);
+      console.log('false');
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-[25px]">Quiz Page</h1>
+    <div className="mx-8">
+      <h1 className="mt-[20px] text-[25px]">Quiz Page</h1>
       <div>
         <h3>
           Question: {activeQuestion + 1}
-          <span>/5</span>
+          <span>/{questions.length}</span>
         </h3>
+      </div>
+      <div>
+        {!showResult ? (
+          <div className="mt-[40px] w-[500px] h-full px-2 py-2 bg-[#e4e4e4] rounded-md">
+            <h3 className="font-[500] text-black text-[25px]">
+              {questions[activeQuestion].question}
+            </h3>
+            {answers.map((answer, index) => (
+              <ul className="text-black mt-3" key={index}>
+                <li
+                  key={index}
+                  onClick={() => onAnswerSelected(answer, index)}
+                  className={`border my-2 ${
+                    selectedAnswerIndex === index
+                      ? 'bg-black text-white'
+                      : 'hover:bg-gray-100'
+                  } py-2 px-2 cursor-pointer h-auto border-gray-300`}
+                >
+                  {answer}
+                </li>
+              </ul>
+            ))}
+            <div className="flex justify-center">
+              {checked ? (
+                <button
+                  // onClick={nextQuestion}
+                  className="bg-gray-700 w-[50%] h-[45px] rounded-md"
+                >
+                  {activeQuestion === question.length - 1
+                    ? 'Finish 🎯'
+                    : 'Next'}
+                </button>
+              ) : (
+                <button
+                  // onClick={nextQuestion}
+                  disabled
+                  className="bg-gray-400 w-[50%] cursor-not-allowed h-[45px]"
+                >
+                  Choose an answer
+                </button>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="w-[300px] h-[30px] bg-white"></div>
+        )}
       </div>
     </div>
   );
