4c88827ae36139185738e642eb732cd0c4616743
EdwinBayingana <bayinganaedwin@gmail.com>
2023-07-19 16:15:11 +0200

    render dummy questions and answers dynamically without proper styles

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,23 +1,137 @@
 'use client';
-import React, { useState, useEffect } from 'react';
-import {
-  QuizLeftSide,
-  QuizRightSide,
-  Container,
-  Navbar,
-  Section,
-} from '@/components';
+import React, { useState } from 'react';
+import { Container, Navbar, Section } from '@/components';
+import { quiz_1 } from '../../data/dummyQuiz';
+
+interface Question {
+  id: number;
+  imageUrl: string;
+  question: string;
+  answers: string[];
+  correctAnswer: string;
+}
+
+const QuizImage = ({ imageUrl }: { imageUrl: string }) => {
+  return (
+    <div className="flex justify-center items-center h-[400px]">
+      {/* Centered Image */}
+      <img
+        src={imageUrl}
+        alt="Quiz Image"
+        className="w-[300px] h-[300px] rounded-lg object-cover"
+      />
+    </div>
+  );
+};
+
+const QuizAnswerOptions = ({
+  answers,
+  selectedAnswer,
+  handleAnswerSelection,
+}: any) => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-4">
+      {/* Answer Options */}
+      {answers.map((answer: string, index: number) => (
+        <div
+          key={index}
+          className={`w-64 h-12 rounded-md mb-2 cursor-pointer ${
+            selectedAnswer === index ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleAnswerSelection(index)}
+        >
+          {answer}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const QuizQuestions = ({
+  questions,
+  selectedQuestion,
+  handleQuestionSelection,
+}: any) => {
+  return (
+    <div className="flex flex-col items-center mt-4">
+      {/* Question Containers */}
+      {questions.map((question: Question) => (
+        <div
+          key={question.id}
+          className={`w-32 h-12 rounded-md mb-2 cursor-pointer ${
+            selectedQuestion === question.id
+              ? 'bg-blue-500 text-white'
+              : 'bg-gray-200'
+          }`}
+          onClick={() => handleQuestionSelection(question.id)}
+        >
+          {`Question ${question.id}`}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const SingleQuizPage = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [selectedQuestion, setSelectedQuestion] = useState<number | null>(null);
+
+  const handleAnswerSelection = (answerIndex: number) => {
+    setSelectedAnswer(answerIndex);
+  };
+
+  const handleQuestionSelection = (questionId: number) => {
+    setSelectedQuestion(questionId);
+  };
+
   return (
     <Container className="text-black">
       <Navbar />
       <Section className="flex">
-        <QuizLeftSide />
-        <QuizRightSide />
+        <div className="w-65">
+          <QuizImage
+            imageUrl={quiz_1.questions[selectedQuestion]?.imageUrl || ''}
+          />
+          <QuizAnswerOptions
+            answers={quiz_1.questions[selectedQuestion]?.answers || []}
+            selectedAnswer={selectedAnswer}
+            handleAnswerSelection={handleAnswerSelection}
+          />
+        </div>
+        <div className="w-35">
+          <QuizQuestions
+            questions={quiz_1.questions}
+            selectedQuestion={selectedQuestion}
+            handleQuestionSelection={handleQuestionSelection}
+          />
+        </div>
       </Section>
     </Container>
   );
 };
 
 export default SingleQuizPage;
+
+// 'use client';
+// import React, { useState, useEffect } from 'react';
+// import {
+//   QuizLeftSide,
+//   QuizRightSide,
+//   Container,
+//   Navbar,
+//   Section,
+// } from '@/components';
+
+// const SingleQuizPage = () => {
+//   return (
+//     <Container className="text-black">
+//       <Navbar />
+//       <Section className="flex">
+//         <QuizLeftSide />
+//         <QuizRightSide />
+//       </Section>
+//     </Container>
+//   );
+// };
+
+// export default SingleQuizPage;
---
+++ src/data/dummyQuiz.ts
@@ -0,0 +1,47 @@
+const quiz_1 = {
+  quiz_title: 'CAR BRANDS',
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      imageUrl:
+        'https://storage.googleapis.com/webdesignledger.pub.network/WDL/d2a6d8d2-t3.jpg',
+      question: '',
+      answers: ['Mercedes', 'Ferrari', 'Tesla', 'Toyota'],
+      correctAnswer: 'Tesla',
+    },
+    {
+      id: 2,
+      imageUrl: 'https://cdn.wallpapersafari.com/12/15/He1SR7.jpg',
+      question: '',
+      answers: ['Mazda', 'Lamborghini', 'Kia', 'Hyundai'],
+      correctAnswer: 'Mazda',
+    },
+    {
+      id: 3,
+      imageUrl:
+        'https://www.bocadecals.com/cdn/shop/products/large-ford-emblem-matte-black-1000_8c8f7471-5ee0-443b-abf4-7bcb15a6c9fd_700x700.jpg?v=1645463400',
+      question: '',
+      answers: ['Mazda', 'Ford', 'Audi', 'Hyundai'],
+      correctAnswer: 'Ford',
+    },
+    {
+      id: 4,
+      imageUrl:
+        'https://hindi.thevocalnews.com/wp-content/uploads/2022/12/byroplhejcqo5mjisf3b.jpg',
+      question: '',
+      answers: ['Hyundai', 'Nissan', 'Kia', 'Ford'],
+      correctAnswer: 'Hyundai',
+    },
+    {
+      id: 5,
+      imageUrl:
+        'https://e0.pxfuel.com/wallpapers/387/569/desktop-wallpaper-honda-logo-logo-honda-emblem.jpg',
+      question: '',
+      answers: ['Jeep', 'Honda', 'Audi', 'Mercedes'],
+      correctAnswer: 'Honda',
+    },
+  ],
+};
+
+export { quiz_1 };
