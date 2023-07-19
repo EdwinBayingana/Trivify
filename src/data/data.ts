e556f9f86c7b52f0bdd0ce28f6b9ba17faa04c2b
EdwinBayingana <bayinganaedwin@gmail.com>
2023-07-19 09:17:42 +0200

    add more objects in the quizData array in data

--- src/data/data.ts
+++ src/data/data.ts
@@ -47,5 +47,96 @@ export const quizData = [
     difficulty: 'Medium',
     category: 'Music',
   },
-  // Add more quiz objects with category property as needed
+  {
+    title: 'Quiz 3',
+    description: 'This is Quiz 3 description',
+    difficulty: 'Hard',
+    category: 'Tv Series',
+  },
+  {
+    title: 'Quiz 4',
+    description: 'This is Quiz 4 description',
+    difficulty: 'Easy',
+    category: 'Movies',
+  },
+  {
+    title: 'Quiz 5',
+    description: 'This is Quiz 5 description',
+    difficulty: 'Medium',
+    category: 'AI',
+  },
+  {
+    title: 'Quiz 6',
+    description: 'This is Quiz 6 description',
+    difficulty: 'Hard',
+    category: 'Software Development',
+  },
+  {
+    title: 'Quiz 7',
+    description: 'This is Quiz 7 description',
+    difficulty: 'Medium',
+    category: 'Tv Series',
+  },
+  {
+    title: 'Quiz 8',
+    description: 'This is Quiz 8 description',
+    difficulty: 'Hard',
+    category: 'AI',
+  },
+  {
+    title: 'Quiz 9',
+    description: 'This is Quiz 9 description',
+    difficulty: 'Easy',
+    category: 'Software Development',
+  },
+  {
+    title: 'Quiz 10',
+    description: 'This is Quiz 10 description',
+    difficulty: 'Medium',
+    category: 'Sports',
+  },
+  {
+    title: 'Quiz 11',
+    description: 'This is Quiz 11 description',
+    difficulty: 'Hard',
+    category: 'Movies',
+  },
+  {
+    title: 'Quiz 12',
+    description: 'This is Quiz 12 description',
+    difficulty: 'Easy',
+    category: 'Music',
+  },
+  {
+    title: 'Quiz 13',
+    description: 'This is Quiz 13 description',
+    difficulty: 'Medium',
+    category: 'AI',
+  },
+  {
+    title: 'Quiz 14',
+    description: 'This is Quiz 14 description',
+    difficulty: 'Hard',
+    category: 'Tv Series',
+  },
+  {
+    title: 'Quiz 15',
+    description: 'This is Quiz 15 description',
+    difficulty: 'Easy',
+    category: 'Software Development',
+  },
+  {
+    title: 'Quiz 16',
+    description: 'This is Quiz 16 description',
+    difficulty: 'Medium',
+    category: 'Sports',
+  },
+  {
+    title: 'Quiz 17',
+    description: 'This is Quiz 17 description',
+    difficulty: 'Hard',
+    category: 'Movies',
+  },
 ];
+
+export default quizData;
